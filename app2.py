7881b79f18f7841486eb3ab2a888be9f652bd31f
Ramakrishnaboina <163442318+Ramakrishnaboina@users.noreply.github.com>
2024-03-18 01:34:51 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,12 +7,12 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/v2/hello')
-def index():
+@app.route('/health')
+def index2():
    print('Health is good')
    return jsonify("Health is good for v2")
 
-def extract_mean(file):
+def extract_mean2(file):
     y, sr = librosa.load(file)
     # Extract audio features
     chroma_stft = np.mean(librosa.feature.chroma_stft(y=y, sr=sr))
@@ -23,27 +23,20 @@ def extract_mean(file):
     zero_crossing_rate = np.mean(librosa.feature.zero_crossing_rate(y=y))
     mfccs = librosa.feature.mfcc(y=y, sr=sr)
     audMeansList = []
-    # Print or use the extracted features
-    #data1=str(np.mean(chroma_stft))+","+str(rms)+","+str(spectral_centroid)+","+str(spectral_bandwidth)+","+str(rolloff)+","+str(zero_crossing_rate)
     length = len(mfccs)
     audMeansList.append(chroma_stft)
     audMeansList.append(rms)
     audMeansList.append(spectral_bandwidth)
     audMeansList.append(rolloff)
     audMeansList.append(zero_crossing_rate)
-    # Iterating the index
-    # same as 'for i in range(len(list))'
-    #data2=","
     for i in range(length):
        audMeansList.append(np.mean(mfccs[i]))
-    #dataAll="["+data1+data2+"]"
-    #print(dataAll)
     return {
 	    'data': [audMeansList]
 	}
 
-@app.route('/v2/extract_audio_features', methods=['POST'])
-def upload_file():
+@app.route('/extract_audio_features2', methods=['POST'])
+def upload_file2():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
 
@@ -58,7 +51,7 @@ def upload_file():
         file.save(temp_audio_file)
 
         # Extract audio features
-        audio_features = extract_mean(temp_audio_file)
+        audio_features = extract_mean2(temp_audio_file)
 
         # Delete the temporary audio file
         os.remove(temp_audio_file)
